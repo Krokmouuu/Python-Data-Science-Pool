1eb1828bf916c87dd8f192a35c1125c02e436379
Benoit Leroy <bleroy@c1r7p3.42nice.fr>
2023-03-16 21:00:12 +0100

    need fix

--- Day00/ex04/whatis.py
+++ Day00/ex04/whatis.py
@@ -7,17 +7,35 @@ def main():
         if (size == 1):
             return 0
         assert size == 2, "more than one argument are provided"
-        if (len(arg.argv) == 2):
-            f = arg.argv[1].isdigit()
-            assert f is True, "argument is not an integer"
-            num = int(arg.argv[1])
-            if (num % 2 == 0):
-                print("I'm Even.")
-            else:
-                print("I'm Odd.")
+        try:
+            if (len(arg.argv) == 2):
+                # f = arg.argv[1].isdigit()
+                # assert f is True, "argument is not an integer"
+                if (arg.argv[1][:0] == '-' or arg.argv[1][:0] == '+'):
+                    num = arg.argv[1][:1].isdigit()
+                    if (num is True):
+                        raise AssertionError("argument is not an integer")
+                    else:
+                        num = int(arg.argv[1][1:])
+                    if (num % 2 == 0):
+                        print("I'm Even.")
+                    else:
+                        print("I'm Odd.")
+                else:
+                    num = arg.argv[1].isdigit()
+                    if (num is True):
+                        raise AssertionError("argument is not an integer")
+                    else:
+                        num = int(arg.argv[1])
+                    if (num % 2 == 0):
+                        print("I'm Even.")
+                    else:
+                        print("I'm Odd.")
+        except AssertionError as msg:
+            print("AssertionError:", msg)
     except AssertionError as msg:
         print("AssertionError:", msg)
-        return
+    return
 
 
 if __name__ == "__main__":
