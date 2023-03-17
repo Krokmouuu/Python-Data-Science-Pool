b810086072a6a7c4ca2e7852fbaff7d2ab6542b4
Benoit Leroy <bleroy@c1r3p1.42nice.fr>
2023-03-17 16:03:52 +0100

    LITTLE FIX

--- Day00/ex03/NULL_not_found.py
+++ Day00/ex03/NULL_not_found.py
@@ -2,13 +2,13 @@ def NULL_not_found(object: any) -> int:
     if (object is None):
         print("Nothing: None", type(object))
         return 0
-    elif (type(object) == float):
+    elif (type(object) == float and object == "NaN"):
         print("Cheese: nan", type(object))
         return 0
     elif (type(object) == bool and object is False):
         print("Fake: False", type(object))
         return 0
-    elif (type(object) == int):
+    elif (type(object) == int and object == 0):
         print("Zero: 0", type(object))
         return 0
     elif (type(object) == str and object == ""):
